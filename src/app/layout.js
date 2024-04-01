ff9c603b6e50e1e06635f8a4bbf97ad8d6451a51
국경훈 <>
2024-04-02 06:31:21 +0900

    Your commit message

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,10 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body>
-                <img src="/images/mainBG.png" className="main-bg" />
+                <header className="header">
+                    <img src="/images/logo1.svg" className="logo" />
+                </header>
+
                 <div className="layout">{children}</div>
             </body>
         </html>
