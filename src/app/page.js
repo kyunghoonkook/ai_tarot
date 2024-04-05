f74bd850f995c874273c810587cf408c1236f646
국경훈 <khkook@ebionet.com>
2024-04-05 13:09:13 +0900

    no message

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import styles from './page.module.css';
 
 function App() {
     return (
-        <div className={styles.bgWrap}>
+        <div className={styles['bgWrap']}>
             <img src="/images/mainBG.png" className={styles['main-bg']} />
             <ThemeSelector />
         </div>
