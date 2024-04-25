004d7df4608a70a64d528da806b54f4133826436
국경훈 <khkook@ebionet.com>
2024-04-25 10:35:43 +0900

    ㄴㅇ

--- src/app/layout.js
+++ src/app/layout.js
@@ -75,7 +75,7 @@ export default function RootLayout({ children }) {
                 <script
                     async
                     src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6444523705828999"
-                    crossOrigin="anonymous"
+                    crossorigin="anonymous"
                 ></script>
             </head>
             <body>
